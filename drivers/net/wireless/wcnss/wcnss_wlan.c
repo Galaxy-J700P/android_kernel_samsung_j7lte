6620157bfc0cde8dae43fd73f55a26d950bfd20b
Sarada Prasanna Garnayak <sgarna@codeaurora.org>
2017-04-17 14:29:57 +0530

    wcnss: fix the potential memory leak and heap overflow
    The wcnss platform driver update the wlan calibration data
    by the user space wlan daemon. The wlan user space daemon store
    the updated wlan calibration data reported by wlan firmware in
    user space and write it back to the wcnss platform calibration
    data buffer for the calibration data download and update.
    
    During the wlan calibration data store and retrieve operation
    there are some potential race condition which leads to memory leak
    and buffer overflow during the context switch.
    
    Fix the above issue by adding protection code and avoid usage of
    global pointer during the device file read and write operation.
    
    CRs-Fixed: 2015858
    Change-Id: Ib5b57eb86dcb4e6ed799b5222d06396eaabfaad3
    Signed-off-by: Sarada Prasanna Garnayak <sgarna@codeaurora.org>

--- drivers/net/wireless/wcnss/wcnss_wlan.c
+++ drivers/net/wireless/wcnss/wcnss_wlan.c
@@ -2653,7 +2653,7 @@ static ssize_t wcnss_ctrl_read(struct file *fp, char __user *user_buffer,
 {
 	int rc = 0;
 
-	if (!penv || !penv->device_opened)
+	if (!penv)
 		return -EFAULT;
 
 	rc = wait_event_interruptible(penv->wlan_config.wcnss_ctrl_wait,
@@ -3385,6 +3385,11 @@ wcnss_wlan_probe(struct platform_device *pdev)
 	mutex_init(&penv->pm_qos_mutex);
 	init_waitqueue_head(&penv->read_wait);
 	init_waitqueue_head(&penv->wlan_config.wcnss_ctrl_wait);
+
+	penv->user_cal_rcvd = 0;
+	penv->user_cal_read = 0;
+	penv->user_cal_exp_size = 0;
+	penv->user_cal_available = false;
 
 	penv->user_cal_rcvd = 0;
 	penv->user_cal_read = 0;
