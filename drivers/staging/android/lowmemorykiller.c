6817bce08b6d02c76285f9ea951d3b5f7e152842
minz1 <emerytang@gmail.com>
2017-05-20 16:13:58 -0400

    staging: android: lowmemorykiller: fix build
    * dump_tasks_info is unused when CONFIG_SEC_OOM_KILLER is disabled;
      make sure that it is enabled before definition

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -204,6 +204,7 @@ static struct notifier_block lmk_vmpr_nb = {
 	.notifier_call = lmk_vmpressure_notifier,
 };
 
+#ifdef CONFIG_SEC_OOM_KILLER
 static void dump_tasks_info(void)
 {
 	struct task_struct *p;
@@ -235,6 +236,7 @@ static void dump_tasks_info(void)
 		task_unlock(task);
 	}
 }
+#endif /*CONFIG_SEC_OOM_KILLER*/
 
 static int test_task_flag(struct task_struct *p, int flag)
 {
